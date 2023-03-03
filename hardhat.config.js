f0bbeb1d3473fc577c6887d9657f2e764e9b6984
shreyansh <shreyanshag2014@gmail.com>
2023-03-03 13:57:59 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks:{
+    goerli:{
+      url: process.env.INFURA_URL,
+      accounts:[process.env.WALLET_PRIVATE_KEY]
+    }
+  }
+};

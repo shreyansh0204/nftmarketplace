98b433c2fe5e5ed174854ebeb436f7ba56636259
shreyansh <shreyanshag2014@gmail.com>
2023-03-19 20:13:56 +0530

    changes updated

---
+++ client/config.js
@@ -0,0 +1,5 @@
+export const contractAddress="0xCe5050945c105C48c859BbC19Bb1BDb2c79ca2Fc";
+export const INFURA_URL="https://goerli.infura.io/v3/2d4988293f16499b907e89d53d6b8c31";
+export const PINATA_KEY="f6d01e8dbd6dc5d6627a";
+export const PINATA_SECRET="8c7d4487970e4184dbda050c1e8e9393a51f49c8fc4eacce6db42411020d5f88";
+
---
+++ client/pages/_app.js
@@ -0,0 +1,28 @@
+import '../styles/globals.css'
+import Link from 'next/link';
+
+function MyApp({ Component,pageProps }){
+  return(
+    <div>
+      <nav className='border-b p-6'>
+        <p className='text-4xl font-bold'>Metaverse NFT Store</p>
+        <div className='flex mt-4'></div>
+        <Link href='/' legacyBehavior>
+          <a className='mr-4 text-pink-500'>Home</a>
+        </Link>
+        <Link href='/create-nft' legacyBehavior>
+          <a className='mr-6 text-pink-500'>Sell NFT</a>
+        </Link>
+        <Link href='/my-nfts' legacyBehavior>
+          <a className='mr-4 text-pink-500'>My NFT</a>
+        </Link>
+        <Link href='/creator-dashboard' legacyBehavior>
+          <a className='mr-4 text-pink-500'>Dashboard</a>
+        </Link>
+      </nav>
+    <Component {...pageProps} />
+      </div>
+  );
+}
+
+export default MyApp
